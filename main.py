7be88ee8dddeca1931b5310939cf4a7be9193cd5
letsa <letsa@users.noreply.github.com>
2026-03-23 16:09:57 -0500

    Bump version to 5.19.4

--- main.py
+++ main.py
@@ -337,7 +337,7 @@ def smart_extract_list(body: dict, field_names: list, default=None):
 # APP
 # ---------------------------------------------------------------------
 
-APP_VERSION = "5.19.3"
+APP_VERSION = "5.19.4"
 
 app = FastAPI(
     title="HyperCore GH-OS ML Service",
